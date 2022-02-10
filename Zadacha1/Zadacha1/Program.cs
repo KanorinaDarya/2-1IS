89c3018a9869c153f65581342d445386742ca223
KanorinaDarya <99387443+KanorinaDarya@users.noreply.github.com>
2022-02-10 11:18:04 +0300

    Add files via upload

---
+++ Zadacha1/Zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Введите Ваше имя. ");
+            name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + "!");
+            Console.ReadKey();
+        }
+    }
+}
