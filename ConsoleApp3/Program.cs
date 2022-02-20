aa0a5710f620263ae1921e665dcd923ae317fe6f
Дарья Канорина <doschik3758@gmail.com>
2022-02-20 23:50:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("myhello");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}");
+            Console.ReadKey();
+        }
+    }
+}
